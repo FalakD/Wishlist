b922a1a51958af785e5f548529623f83e8cbafd3
FalakD <30274347+FalakD@users.noreply.github.com>
2017-07-26 21:00:07 -0700

    Initial commit

---
+++ app/src/main/java/com/example/practicalapp/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.practicalapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button registerBtn = (Button) findViewById(R.id.register);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
